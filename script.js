e890621a87198815ad17e191c46ede30ca102f8e
NikitaOtr <nikita137174@list.ru>
2021-08-04 14:40:49 +0300

    init

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 'use strict';
 
-const str = prompt('Введите строку');
+const arr = ['4223234', '232434', '34324324', '932445434', '23646564533', '46254234535', '752345325'];
+for (let item of arr) {
+     if (item[0] === '2' || item[0] === '4'){
+          console.log(item);
+     }
+}
 
-const fixText = function(string) {
-     string = string.trim();
-     return string.length > 30 ? string.slice(0, 30) + '...' : string; 
+const checkPrime = function(n) {
+     if (!Number.isInteger(n)) {
+          return false;
+     }
+     if (n < 2) {
+          return false;
+     }
+     for (let i = 2; i <= Math.sqrt(n); i++){
+          if (n % i === 0){
+               return false;
+          }
+     }
+     return true;
 };
 
-console.log(fixText(str));
+let dev;
+for (let num = 0; num < 101; num++) {
+     if (checkPrime(num)){
+          console.log(num);
+     }
+}
