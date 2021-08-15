31b2f78ca9de8317a18c7536d6c13e867ca537b3
NikitaOtr <nikita137174@list.ru>
2021-08-15 20:17:25 +0300

    init

--- script.js
+++ script.js
@@ -1,36 +1,25 @@
 'use strict';
-
-const time = new Date();
-
-setInterval(function () {
-      time.setSeconds(time.getSeconds() + 1);
-      createClockA(time);
-      createClockB(time);
-}, 1000);
-
-
-const clockA = document.createElement('div');
-createClockA(time);
-document.body.appendChild(clockA);
-
-function createClockA(time2) {
-      const options = {
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric',
-            weekday: 'long',
-            hour: 'numeric',
-            minute: 'numeric',
-            second: 'numeric'
-            };
-      clockA.innerHTML = 'Cегодня ' + time2.toLocaleString('ru', options);
+function DomElement(selector = '.block', height = 100, width = 100, bg = 'white', fontSize = '16px') {
+    this.selector = selector; 
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
 }
+DomElement.prototype.bildElement = function() {
+    const block = document.createElement('div');
+    block.textContent = 'Hello world!!!';
+    if (this.selector.startsWith('.')) {block.classList.add(this.selector.slice(1));}
+    if (this.selector.startsWith('#')) {block.id = this.selector.slice(1);}
+    block.style.cssText = 'background: ' + this.bg;
+    block.style.cssText += 'height: ' + this.height + 'px';
+    block.style.cssText += 'width: ' + this.width + 'px';
+    block.style.cssText += 'font-size: ' + this.fontSize + 'px';
+    return block;
+};
 
+const div1 = new DomElement('#selc', 500, 600, 'red', 70);
+document.body.append(div1.bildElement());
 
-const clockB = document.createElement('div');
-createClockB(time);
-document.body.appendChild(clockB);
-
-function createClockB(time2) {
-      clockB.innerHTML = time2.toLocaleString().split(', ').join(' - ');
-}
+const div2 = new DomElement('.div', 60, 60, 'green', 15);
+document.body.append( div2.bildElement());
