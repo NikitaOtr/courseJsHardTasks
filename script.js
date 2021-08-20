e2ff0169c4074009d41ce01e757bdd7ca5b8d42d
NikitaOtr <nikita137174@list.ru>
2021-08-20 13:40:05 +0300

    init

--- script.js
+++ script.js
@@ -1,24 +1,52 @@
-const div = document.querySelector('div');
-const button = document.querySelector('button');
 
-let move = false;
-let count = 0;
-let left;
+//debounce
+const input = document.querySelector('input');
+const p = document.querySelector('p');
 
-
-function moveDiv() {
-    console.log(count);
-    div.style.left = count + 'px';
-    count++;
-    left = requestAnimationFrame(moveDiv);
+function debounce(fn, time) {
+    let timeoutFlag;
+    return function(...arg) {
+        const fnCall = () => fn.apply(this, arg);
+        clearTimeout(timeoutFlag);
+        timeoutFlag = setTimeout(fnCall, time, ...arg);
+    };
 }
 
-button.addEventListener('click', () => {
-    if (!move) {
-        left = requestAnimationFrame(moveDiv);
-        move = true;
-    } else {
-        move = false;
-        cancelAnimationFrame(left);
-    }
-});
+function onChagne(e) {
+    p.textContent = e.target.value;
+}
+
+input.addEventListener('keyup', debounce(onChagne, 200));
+
+
+//Throttle
+const consoleCoorrinate = function(e) {
+    console.log(this);
+    p.textContent = e.clientX + ' ' + e.clientY;
+};
+
+const throttle = (fn, time) => {
+    let isThrottled = false;
+    let saveThis;
+    let saveArgs;
+
+    return function wrapper(...arg) {
+        if (isThrottled) {
+            saveThis = this;
+            saveArgs = arg;
+            return;
+        }
+
+        fn.apply(this, arg);
+        isThrottled = true;
+        setTimeout(() => {
+            isThrottled = false;
+            if (saveThis) {
+                wrapper.apply(saveThis, saveArgs);
+                saveArgs = saveThis = null;
+            }
+        }, time);
+    };
+};
+
+document.addEventListener('mousemove', throttle(consoleCoorrinate, 5000));
